3f7037b4eecd6030259395a68a4d2aa879c620e7
HootiePumpkin <44407057+HootiePumpkin@users.noreply.github.com>
2018-11-29 21:42:36 +0200

    Add files via upload

--- Robot/oneservotest/test.py
+++ Robot/oneservotest/test.py
@@ -4,7 +4,7 @@ from time import sleep
 
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(03, GPIO.OUT)
+GPIO.setup(3, GPIO.OUT)
 
 pwm = GPIO.PWM(3, 50)
 
@@ -13,11 +13,11 @@ pwm.start(0)
 def SetAngle(angle):
 
 	duty = angle / 18 + 2
-	GPIO.output(03, True)
+	GPIO.output(3, True)
 	pwm.ChangeDutyCycle(duty)
 	sleep(1)
 	
-	GPIO.output(03, False)
+	GPIO.output(3, False)
 	pwm.ChangeDutyCycle(0)
 	
 SetAngle(90)
